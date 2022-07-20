118aad68b746c6bfd8cbaf619fbe4bc704fdc3b7
wdewfef <20022002utkarsh@gmail.com>
2022-07-20 14:38:11 +0530

    added hotstar clone

---
+++ data.js
@@ -0,0 +1,27 @@
+let movies=[
+    {
+        name:'Falcon And The Winter Soldier',
+        des: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Et voluptatem dolorem tempora in repellendus?',
+        image:'/images/slider 2.PNG'
+    },
+   {
+    name:'Loki',
+    des:'Lorem ipsum dolor sit amet consectetur adipisicing elit. Et voluptatem dolorem tempora in repellendus?',
+    image:'/images/slider 1.PNG'
+   },
+   {
+    name:'Wanda Vision',
+    des:'Lorem ipsum dolor sit amet consectetur adipisicing elit. Et voluptatem dolorem tempora in repellendus?',
+    image:'/images/slider 3.PNG'
+   },
+   {
+    name:'Raya and the Last Dragon',
+    des: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Et voluptatem dolorem tempora in repellendus?',
+    image:'/images/slider 4.PNG'
+   },
+   {
+    name:'Luca',
+    des:'Lorem ipsum dolor sit amet consectetur adipisicing elit. Et voluptatem dolorem tempora in repellendus?',
+    image:'/images/slider 5.PNG'
+   },
+];
